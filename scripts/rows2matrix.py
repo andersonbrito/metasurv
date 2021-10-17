4bca9751a960d2a69ba08d40fdf9bd40b711bfc3
Anderson Brito <andersonfbrito@gmail.com>
2021-10-17 00:46:10 -0300

    Update rows2matrix.py

--- scripts/rows2matrix.py
+++ scripts/rows2matrix.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     parser.add_argument("--xtype", required=False, type=str, help="Is the x variable a time variable (date)? If so, enter 'time'")
     parser.add_argument("--target", required=False, type=str, help="Target column, when variable is already aggregated")
     parser.add_argument("--yvar", required=True, type=str, help="Data that goes in the Y axis of the matrix")
-    parser.add_argument("--extra-columns", required=False, nargs='+', type=str, help="extra columns to exported")
+    parser.add_argument("--extra-columns", required=False, nargs='+', type=str, help="extra columns to export")
     parser.add_argument("--filter", required=False, type=str, help="Format: '~column_name:value'. Remove '~' to keep only that data category")
     parser.add_argument("--start-date", required=False, type=str,  help="Start date in YYYY-MM-DD format")
     parser.add_argument("--end-date", required=False, type=str,  help="End date in YYYY-MM-DD format")
@@ -37,23 +37,22 @@ if __name__ == '__main__':
     x_type = args.xtype
     target_variable = args.target
     y_var = args.yvar
-    extra_cols = args.extra_columns
+    extra_cols = args.extra_columns[0]
     filter_value = args.filter
     start_date = args.start_date
     end_date = args.end_date
     output = args.output
 
-
-    # path = '/Users/anderson/GLab Dropbox/Anderson Brito/ITpS/projetos_itps/dashboard/nextstrain/run5_20210921_itps2/ncovbr/data/'
-    # input = path + 'metadata.tsv'
+    # path = '/Users/anderson/GLab Dropbox/Anderson Brito/ITpS/projetos_itps/metasurvBR/analyses/bubbles_20211016/'
+    # input = path + 'caso_full.csv'
     # x_var = 'date'
     # x_type = 'time'
-    # target_variable = ''
-    # y_var = 'variant_lineage'
-    # extra_cols = 'country'
-    # filter_value = 'country:Brazil-North'
-    # start_date = '2021-07-25' # start date above this limit
-    # end_date = '2021-09-18' # end date below this limit
+    # target_variable = 'new_confirmed'
+    # y_var = 'state'
+    # extra_cols = 'city_ibge_code, estimated_population_2019'
+    # filter_value = 'place_type:state'
+    # start_date = '2021-08-29' # start date above this limit
+    # end_date = '2021-10-09' # end date below this limit
     # output = path + 'matrix.tsv'
 
 
@@ -103,19 +102,43 @@ if __name__ == '__main__':
         cols = sorted(df[x_var].unique().tolist())
     rows = sorted(df[y_var].unique().tolist())
 
+
     # print(cols)
     # print(rows)
 
     df2 = pd.DataFrame(index=rows, columns=cols)
     df2 = df2.fillna(0) # with 0s rather than NaNs
 
+    # add other columns, if available
+    if extra_cols == None:
+        extra_cols = []
+    else:
+        extra_cols = [x.strip() for x in extra_cols.split(',')]
+
+    for column in extra_cols:
+        if column in df.columns.to_list():
+            df2.insert(0, column, '')
+
     # give index a name
     df2.index.name = y_var
     if target_variable in ['', None]:
         df = df.groupby([x_var, y_var]).size().to_frame(name='count').reset_index() # group and count occorrences
     else:
-        df = df[[x_var, y_var, target_variable]]
+        # df = df[[x_var, y_var, target_variable]]
         df = df.rename(columns={target_variable: 'count'})
+
+    df.set_index(y_var, inplace=True)
+
+    # fill extra columns with their original content
+    for idx, row in df2.iterrows():
+        for column in extra_cols:
+            # print(df.columns.to_list())
+            if column in df.columns.to_list():
+                value = df.loc[idx, column][0]
+                # value = df.loc[df.index == idx][column].values[0]
+                df2.at[idx, column] = value
+
+    df = df.reset_index()
 
     # populate output dataframe
     for idx, row in df.iterrows():
@@ -124,9 +147,9 @@ if __name__ == '__main__':
         count = df.loc[idx, 'count']
         df2.at[y, x] = count
 
-    if filter_value not in ['', None]:
-        df2.insert(0, filter_value.split(':')[0].strip(), '')
-        df2[filter_value.split(':')[0].strip()] = filter_value.split(':')[1].strip()
+    # if filter_value not in ['', None]:
+    #     df2.insert(0, filter_value.split(':')[0].strip(), '')
+    #     df2[filter_value.split(':')[0].strip()] = filter_value.split(':')[1].strip()
 
     # save
     df2.to_csv(output, sep='\t', index=True)
